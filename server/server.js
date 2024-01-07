aa6c4a541844ec2b9b9777e79737d2b06883510f
Vedant Taak <vedanttaak7@gmail.com>
2024-01-07 17:27:35 +0530

    new render1

--- server/server.js
+++ server/server.js
@@ -13,12 +13,12 @@ app.use('/api/admin', adminRoute)
 app.use('/api/doctor', doctorRoute);
 const port = process.env.PORT || 5000
 
-if(process.env.NODE_ENV === 'production'){
-    app.use('/', express.static('../client/build'));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'../client/build/index.html'));
-    })
-}
+// if(process.env.NODE_ENV === 'production'){
+//     app.use('/', express.static('../client/build'));
+//     app.get('*',(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'../client/build/index.html'));
+//     })
+// }
 
 app.get("/", (req,res)=>{
     res.json("Server");
